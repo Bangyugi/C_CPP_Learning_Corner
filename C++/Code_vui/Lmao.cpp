711f8d2971284ee4207582aa149c10c89d981275
Bang Van <bangtranvan08@gmail.com>
2024-01-25 21:48:15 +0700

    update

--- C++/Code_vui/Lmao.cpp
+++ C++/Code_vui/Lmao.cpp
@@ -1,34 +1,26 @@
-    #include <bits/stdc++.h>
-    #define ll long long
-    #define ii pair<int, int>
-    using namespace std;
-    ll MOD = 1e9 + 7;
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<int,int>
+using namespace std;
+ll MOD = 1e9 + 7;
 
-    vector<int> matrix[100005];
-    
-    int main()
-    {
-        // ios_base::sync_with_stdio(NULL);
-        // cin.tie(0);
-        // cout.tie(0);
-        int n, m;
-        cin >> n;
-        vector<vector<int>> v2(n);
-        for (int i = 0; i < n;i++)
-        {
-            for (int j = 1; j <= n;j++)
-            {
-                v2[i].push_back(j);
-            }
-        }
-        for (int i = 0; i < n;i++)
-        {
-            for (auto j: v2[i])
-            {
-                cout << j << " ";
-            }
-            cout << endl;
-        }
-        
-        return 0;
-    }
+int main()
+{
+    //ios_base::sync_with_stdio(NULL);
+    //cin.tie(0);
+    //cout.tie(0);
+    int n;
+    cin >> n;
+    cin.ignore();
+    char c[100];
+    // char s[100];
+    // getline(cin, c);
+    cin.getline(c, 100);
+    // cin.ignore();
+    // cin.getline(s, 100);
+    // getline(cin, s);
+    cout << n << endl;
+    cout << c << endl;
+    // cout << s << endl;
+    return 0;
+}
